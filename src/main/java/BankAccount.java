a232f30a5207d7d4792d1953d03af6b496605ed5
Kavi Ravuri <kaviravuri888@gmail.com>
2017-10-24 14:29:10 -0500

    Finished the getter and setter methods.

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 /**
  * Class implementing a bank account.
  * <p>
@@ -12,27 +10,151 @@ public class BankAccount {
     /*
      * You may want to use this to distinguish between different kinds of accounts.
      */
+    /**
+     *  the account info.
+     */
     public enum BankAccountType {
+        /**
+         * checking.
+         */
         CHECKINGS,
+        /**
+         * savings.
+         */
         SAVINGS,
+        /**
+         * student.
+         */
         STUDENT,
+        /**
+         * work place.
+         */
         WORKPLACE
     }
 
+    /**
+     * the accountNumber.
+     */
     private int accountNumber;
-    public BankAccountType accountType;
+    /**
+     * the accountType.
+     */
+    private BankAccountType accountType;
+    /**
+     * the accountBalance.
+     */
     private double accountBalance;
+    /**
+     * the ownerName.
+     */
     private String ownerName;
-    public double interestRate;
+    /**
+     * the interestRate.
+     */
+    private double interestRate;
+    /**
+     * the interestEarned.
+     */
     private double interestEarned;
 
+    /**
+     * this is random.
+     */
+    private final int random = 10000;
+    /**
+     * this is random.
+     */
+    private final double randomd = 1.5;
+    /**
+     * this is random.
+     */
+    private final double randomd2 = 105.5;
+    /**
+     * @param name is given
+     * @param accountCategory is given
+     */
     public BankAccount(final String name, final BankAccountType accountCategory) {
         /*
          * Implement this function
          */
+        ownerName = name;
+        accountType = accountCategory;
+        interestRate = randomd;
+        interestEarned = randomd2;
+        accountBalance = random;
     }
 
-    /*
-     * Implement getters and setters as appropriate for private variables.
+    /**
+     * @return accountNumber
      */
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+    /**
+     * @return accountType
+     */
+    public BankAccountType getBankAccountType() {
+        return accountType;
+    }
+    /**
+     * @return getaccountBalance
+     */
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+    /**
+     * @return getownerName
+     */
+    public String getOwnerName() {
+        return ownerName;
+    }
+    /**
+     * @return getinterestRate
+     */
+    public double getInterestRate() {
+        return interestRate;
+    }
+    /**
+     * @return getinterestEarned
+     */
+    public double getInterestEarned() {
+        return interestEarned;
+    }
+
+    /**
+     * @param p account number
+     */
+    public void setAccountNumber(final int p) {
+         accountNumber = p;
+    }
+    /**
+     * @param v s the BankAccountType.
+     */
+    public void setBankAccountType(final BankAccountType v) {
+        accountType = v;
+    }
+    /**
+     * @param i is amount
+     */
+    public void setaccountBalance(final double i) {
+        accountBalance = i;
+    }
+    /**
+     * @param n is the name.
+     */
+    public void setownerName(final String n) {
+        ownerName = n;
+    }
+    /**
+     * @param n is he interest rate.
+     */
+    public void setinterestRate(final double n) {
+        interestRate = n;
+    }
+    /**
+     * @param n is the interest earned.
+     */
+    public void setinterestEarned(final double n) {
+        interestEarned = n;
+    }
 }
